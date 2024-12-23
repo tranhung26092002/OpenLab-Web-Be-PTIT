290486d59a7e3567abcdc42d6f21c88703109037
tranhung26092002 <hungtran2609>
2024-12-23 18:50:32 +0700

    feat: fix cors v2

--- src/main/java/com/ptit/service/config/CorsConfig.java
+++ src/main/java/com/ptit/service/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+              .allowedOrigins("http://14.225.255.177:4000") // Gốc chính xác
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
       }
